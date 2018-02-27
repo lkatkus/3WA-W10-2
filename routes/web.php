b9a99ff17f2e813139c540fd843c26b7f4754c25
lkatkus <laimonas@katkus.eu>
2018-02-27 16:32:02 +0200

    Added auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// ADDING A CONTROLLER TO ROUTE
+Route::get('/', 'HomeController@index');
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
